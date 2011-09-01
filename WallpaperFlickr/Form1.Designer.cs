66b4eacdfd93d9c0f45b776a24a9a8715f4e027d
Chris Rae <clr@chrisrae.com>
2011-09-01 17:40:16 +0000

    Fixing setup project to use active config of WallpaperFlickr project, and making the enter key close the main dialog.

--- WallpaperFlickr/Form1.Designer.cs
+++ WallpaperFlickr/Form1.Designer.cs
@@ -390,6 +390,7 @@ namespace WallpaperFlickr {
             // 
             // Form1
             // 
+            this.AcceptButton = this.btnOK;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(460, 390);
