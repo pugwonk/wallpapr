ad56bc1f002fddbad0f8b0690c4159722aa3d09a
Al <alski@users.noreply.github.com>
2012-10-09 22:35:21 +0000

    Added a prototype of the UI concept

---
+++ Branches/UIRework/ReworkDesignTemp/View/LabelledImage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ReworkDesignTemp.View
+{
+    /// <summary>
+    /// Interaction logic for LabelledImage.xaml
+    /// </summary>
+    public partial class LabelledImage : UserControl
+    {
+        public static readonly DependencyProperty ImageProperty = DependencyProperty.Register(
+            "Image", typeof(ImageSource), typeof(LabelledImage));
+
+        public LabelledImage()
+        {
+            InitializeComponent();
+        }
+    }
+}
