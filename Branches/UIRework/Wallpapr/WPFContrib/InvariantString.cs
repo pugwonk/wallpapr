0f15572ef3fb85080aae6d6e14fe8e500518aad9
Al <alski@users.noreply.github.com>
2012-10-09 22:53:44 +0000

    Added WPF NotifyIcon as blatently ripped out of WPFcontrib.CodePlex.com

---
+++ Branches/UIRework/Wallpapr/WPFContrib/InvariantString.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+
+namespace Avalon.Internal.Utility
+{
+    internal static class InvariantString
+    {
+        public static string Format(string format, params object[] args)
+        {
+            return string.Format(CultureInfo.InvariantCulture, format, args);
+        }
+    }
+}
